34ac214e323d8585ed3e259f87dabc78edc89da6
ishotjr <github@ishotjr.com>
2020-08-29 20:58:27 -0400

    :straight_ruler: :1234: Add Distance states

--- OneWayWillie.ino
+++ OneWayWillie.ino
@@ -1,26 +1,47 @@
-const int piezo = 5;
+enum Distance: byte {
+  FAR,
+  NEAR,
+  NEARER,
+  NEAREST
+};
 
-void setup(){
+const int piezo = 5;
+int proximity = FAR;
+
+void setup() {
  
   Serial.begin(9600);
   pinMode(piezo, OUTPUT);
 
 }
 
-void loop(){
+void loop() {
 
   // Sharp GP2Y0A60SZLF Analog Distance Sensor 10-150cm, 5V
   int sensorValue = analogRead(A0);
+  Serial.print("sensorValue: ");
   Serial.println(sensorValue);
 
-  // buzz when too close, escalating pitch with proximity
-  if (sensorValue > 210) {
-    tone(piezo, sensorValue);
+  if (sensorValue > 800) {
+    proximity = NEAREST;
+  } else if (sensorValue > 400) {
+    proximity = NEARER;
+  } else if (sensorValue > 220) {
+    proximity = NEAR;
+  } else {
+    proximity = FAR;
+  }
+  Serial.print("proximity: ");
+  Serial.println(proximity);
+
+  // buzz when too close, escalating pitch and speed with proximity
+  if (proximity > 0) {
+    tone(piezo, proximity * 220, 30 * proximity * proximity);
   } else {
     noTone(piezo);
   }
 
-  // for sensor, not tone (which uses own 
-  delay(100);
+  // for sensor, not tone (which uses own timer)
+  delay(250);
 
 }
