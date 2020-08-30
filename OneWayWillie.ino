bd53068d52eab9390db370585f686409d11ecd0d
ishotjr <github@ishotjr.com>
2020-08-30 01:41:27 -0400

    :no_entry_sign: :back: Ignore movement starting behind sensor

--- OneWayWillie.ino
+++ OneWayWillie.ino
@@ -30,13 +30,15 @@ enum Distance: byte {
   FAR,
   NEAR,
   NEARER,
-  NEAREST
+  NEAREST,
+  BEHIND
 };
-const int distanceColors[] = {0xFFE0,0xFFE0,0xF81F,0xF800};
+const int distanceColors[] = {YELLOW, YELLOW, MAGENTA, RED, RED};
 
 const int piezo = 5;
 int proximity = FAR;
-
+int priorProximity = BEHIND;
+int priorScreen = CYAN;
 
 void setup() {
 
@@ -46,7 +48,7 @@ void setup() {
   Serial.println("Initializing...");
 
   display.begin();
-  display.fillScreen(WHITE);
+  display.fillScreen(priorScreen);
 
 }
 
@@ -57,7 +59,9 @@ void loop() {
   Serial.print("sensorValue: ");
   Serial.println(sensorValue);
 
-  if (sensorValue > 800) {
+  if (sensorValue == 1023) {
+    proximity = BEHIND;
+  } else if (sensorValue > 800) {
     proximity = NEAREST;
   } else if (sensorValue > 400) {
     proximity = NEARER;
@@ -69,13 +73,28 @@ void loop() {
   Serial.print("proximity: ");
   Serial.println(proximity);
 
-  drawFace(proximity);
+
+  // ignore movement starting behind sensor
+  if (proximity == BEHIND) {
+    priorProximity = BEHIND;
+  } else if (proximity < priorProximity) {
+    priorProximity = proximity;
+  }  
+  
+  Serial.print("priorProximity: ");
+  Serial.println(priorProximity);
+  
   
   // buzz when too close, escalating pitch and speed with proximity
-  if (proximity > 0) {
-    tone(piezo, proximity * 220, 30 * proximity * proximity);
-  } else {
+  // but only movement toward sensor
+  if (priorProximity > 0) {
     noTone(piezo);
+    drawFace(BEHIND);
+  } else {
+    if (proximity > 0) {
+      tone(piezo, proximity * 220, 30 * proximity * proximity);
+    }
+    drawFace(proximity);
   }
 
   // for sensor, not tone (which uses own timer)
@@ -87,32 +106,53 @@ void drawFace(byte distance) {
   
   const uint16_t eyeRadius = 4;
   const uint16_t mouthRadius = 6;
+
+  if (distance == BEHIND) {
+
+    if (priorScreen != BLACK) {
+      display.fillScreen(BLACK);
+      priorScreen = BLACK;
+    }
+    
+    display.setCursor(2,2);
+    display.setTextColor(distanceColors[distance]);
+    display.setTextSize(2);
+    display.println("DO NOT ENTER");
+    
+  } else {
+    
+    if (priorScreen != WHITE) {
+      display.fillScreen(WHITE);
+      priorScreen = WHITE;
+    }
+      
+    // head
+    display.fillCircle(display.width() / 2, display.height() / 2, display.height() / 2, distanceColors[distance]);
   
-  // head
-  display.fillCircle(display.width() / 2, display.height() / 2, display.height() / 2, distanceColors[distance]);
-
-  // eyes
-  display.fillCircle(display.width() / 3, 2 * display.height() / 5, eyeRadius, BLACK);
-  display.fillCircle(2 * display.width() / 3, 2 * display.height() / 5, eyeRadius, BLACK);
-
-  if (distance == NEAREST) {
-    // mask eyes to look angry
-    display.fillTriangle(display.width() / 3, 2 * display.height() / 5 - eyeRadius, 
-    2 * display.width() / 3, 2 * display.height() / 5 - eyeRadius, 
-    display.width() / 2, display.height() / 2, distanceColors[distance]);
-  }
+    // eyes
+    display.fillCircle(display.width() / 3, 2 * display.height() / 5, eyeRadius, BLACK);
+    display.fillCircle(2 * display.width() / 3, 2 * display.height() / 5, eyeRadius, BLACK);
   
-  // mouth
-  display.fillCircle(display.width() / 2, 4 * display.height() / 5, mouthRadius, BLACK);
-
-  if ((distance == NEAREST) || (distance == NEARER)) {
-    // mask mouth circle to look worried
-    display.fillRect(display.width() / 2 - mouthRadius, 4 * display.height() / 5, 
-      mouthRadius * 2 + 1, mouthRadius + 1, distanceColors[distance]);
-  } else if (distance == FAR) {
-    // mask mouth circle to create smile
-    display.fillRect(display.width() / 2 - mouthRadius, 4 * display.height() / 5 - mouthRadius, 
-      mouthRadius * 2 + 1, mouthRadius, distanceColors[distance]);
+    if (distance == NEAREST) {
+      // mask eyes to look angry
+      display.fillTriangle(display.width() / 3, 2 * display.height() / 5 - eyeRadius, 
+      2 * display.width() / 3, 2 * display.height() / 5 - eyeRadius, 
+      display.width() / 2, display.height() / 2, distanceColors[distance]);
+    }
+    
+    // mouth
+    display.fillCircle(display.width() / 2, 4 * display.height() / 5, mouthRadius, BLACK);
+  
+    if ((distance == NEAREST) || (distance == NEARER)) {
+      // mask mouth circle to look worried
+      display.fillRect(display.width() / 2 - mouthRadius, 4 * display.height() / 5, 
+        mouthRadius * 2 + 1, mouthRadius + 1, distanceColors[distance]);
+    } else if (distance == FAR) {
+      // mask mouth circle to create smile
+      display.fillRect(display.width() / 2 - mouthRadius, 4 * display.height() / 5 - mouthRadius, 
+        mouthRadius * 2 + 1, mouthRadius, distanceColors[distance]);
+    }
+    
   }
   
 }
